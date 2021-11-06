7707a375086de4952a9ad40695082571e8c659e0
HJ <u07032@daum.net>
2021-11-06 22:23:16 +0900

    +

--- lib/design/drawer.dart
+++ lib/design/drawer.dart
@@ -20,7 +20,6 @@ class _drawerMenuState extends State<drawerMenu> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     authState = FirebaseAuth.instance.authStateChanges();
   }
